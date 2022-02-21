87d9b1a8b33e4f12ba9ddf202e9c2a62a966262c
Mark-Zinger <lllessive@gmail.com>
2022-02-22 01:00:03 +0700

    add styles to form-select

--- src/components/FormSelect/style.ts
+++ src/components/FormSelect/style.ts
@@ -11,8 +11,39 @@ const StyledSelect = styled(Select)(
     
     .formSelect {
       &__control {
-        height: ${vh(55)};
+        min-height: ${vh(55)};
         border: none;
+        &--menu-is-open {
+          border-bottom-left-radius: 0;
+          border-bottom-right-radius: 0;
+          border-bottom: 3px solid;
+          z-index: 2;
+        }
+        &, &:hover {
+          border-color: ${theme.colors.secondary};
+        }
+      }
+      &__indicator {
+        color: ${theme.colors.primary};
+        transition: color .3s ease;
+        cursor: pointer;
+        &:hover {
+          color: ${theme.colors.primary};
+        }
+      }
+      &__indicator-separator {
+        display: none;
+      }
+      &__menu {
+        top: 85%;
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+        padding-top: 0;
+        color: ${theme.colors.grey};
+      }
+      &__option--is-selected {
+        background: ${theme.colors.selectHover};
+        color: inherit;
       }
     }
   `
